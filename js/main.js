2c0dfb18001b672eb377e83edeb7a28e75531adb
Ahmadsabili <ahmadsabili0081@gmail.com>
2021-11-15 17:48:47 +0700

    first commit

--- js/main.js
+++ js/main.js
@@ -1,46 +1,54 @@
-var dots = [' ',' ',' ']
-var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 80,
-    speed:1500,
-    centeredSlides: true,
-    loop: true,
-    autoplay: {
-      delay: 3500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-        renderBullet: function (index,className){
-            return '<span class="' + className + ' ">' +(dots[index]) + '</span>';
-        },
-    },
+ var dots = [' ',' ',' ']
+  var swiper = new Swiper(".mySwiper", {
+      spaceBetween: 80,
+      speed:1500,
+      centeredSlides: true,
+      loop: true,
+      autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+          renderBullet: function (index,className){
+              return '<span class="' + className + ' ">' +(dots[index]) + '</span>';
+          },
+      },
+      on:{
+        slideChangeTransitionStart: function () {
+          gsap.from('.text__slider',{
+            duration:2.5,
+            y:-100,
+            opacity:0,
+            });
+        }
+      },
+});
+    
+  // hambuger nav
+  const hamburger = document.getElementById('hamburger');
+  hamburger.addEventListener('click',function (e){
+      const nav = document.querySelector('nav');
+      nav.classList.toggle('show');
+      hamburger.classList.toggle('rotate');
   });
   
-// hambuger nav
-const hamburger = document.getElementById('hamburger');
-const blur = document.getElementById('blur');
-hamburger.addEventListener('click',function (e){
-    const nav = document.querySelector('nav');
-    nav.classList.toggle('show');
-    hamburger.classList.toggle('rotate');
-});
-
-// btnscroll
-window.addEventListener('scroll',function (){
-  const btn = document.querySelector('.btn');
-  btn.classList.toggle('BtnScroll', window.scrollY > 0);
-});
-function BtnScrolll(){
-  document.body.scrollTop = 0;
-
-  document.documentElement.scrollTop = 0;
-}
-// animmation
-  AOS.init({
-    offset: 120, 
-    delay: 0, 
-    duration: 400, 
-    easing: 'ease',
+  // btnscroll
+  window.addEventListener('scroll',function (){
+    const btn = document.querySelector('.btn');
+    btn.classList.toggle('BtnScroll', window.scrollY > 0);
   });
+  function BtnScrolll(){
+    document.body.scrollTop = 0;
   
+    document.documentElement.scrollTop = 0;
+  }
+  // animmation
+    AOS.init({
+      offset: 120, 
+      delay: 0, 
+      duration: 400, 
+      easing: 'ease',
+    });
+
